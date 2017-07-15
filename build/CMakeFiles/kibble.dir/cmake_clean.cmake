753d0c7f86bfd6b3b05f58f554ffec20583da4bd
= <ethanmacdonald04@gmail.com>
2017-07-15 13:16:56 -0300

    initial commit

---
+++ build/CMakeFiles/kibble.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kibble.dir/kibble/main.c.o"
+  "kibble.pdb"
+  "kibble"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kibble.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/kibble/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    puts("n' catsn'");
+    return 0;
+}
